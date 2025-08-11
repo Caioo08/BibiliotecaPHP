f5b01932f6f032d94b24475c4153606b80931ecd
231098 <231098@CPD03-M16>
2025-08-11 15:55:10 -0300

    11/8

--- APP/View/categoria/form_categoria.php
+++ APP/View/categoria/form_categoria.php
@@ -1,29 +1,38 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistemas Bibliotecas - Cadastro de Categoria</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistemas Biblioteca - Cadastro de Categorias</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
+
     <div>
         <?php include VIEWS . '/Includes/menu.php' ?>
-        <h1>Cadastro de Categorias</h1>
+
+        <h1>Cadastro de Categoria</h1>
+
         <?= $model->getErrors() ?>
 
-        <form action="/aluno/cadastro" method="post" class="p-5">
-            <input type="hidden" name="id" value="<?= $model->Id ?>">
-
-            <div class="mb-3">
-                <label for="descricao" class="form-label">Categoria:</label>
-                <input type="text" class="form-control" value="<?= $model->descricao ?>" name="descricao" id="descricao">
-            </div>
+        <form method="post" action="/categoria/cadastro" class="p-5">
             
+            <input name="id" type="hidden" value="<?= $model->Id ?>" /> 
+           
+            <div class="mb-3">
+                <label for="descricao" class="form-label">Descrição:</label>
+                <input type="text" value="<?= $model->Descricao ?>" class="form-control" name="descricao" id="descricao">
+            </div>
+          
+            <button type="submit" class="btn btn-success">Salvar</button>
+
         </form>
 
     </div>
-    
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
